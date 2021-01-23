16f191c1fadd04aae196462a101a19fb010ece15
johan31220 <58666354+johan31220@users.noreply.github.com>
2021-01-23 22:13:27 +0100

    Add files via upload

--- js/gestionnaireScript.js
+++ js/gestionnaireScript.js
@@ -1,2 +1,80 @@
 $( document ).ready(function() {
+
+	//Modifier la copropriete que le gestionnaire va choisir
+	$('.changerCoproprieteCheck').on('click',function(){
+
+		idCopropriete = $(this).attr('value')
+		console.log(idCopropriete)
+		$.ajax({
+			url: 'model/gestionnaireModel.php',
+			type: 'POST',
+			data: 'idCopropriete='+ idCopropriete,
+			success : function(response){
+				window.location.reload()
+			},
+			error : function(response){
+				console.log(response)
+			}
+		})
+
+
+	})
+
+	/* Click sur bouton supprimer d'un copropriétaire*/
+	$('.supprimerCoproprietaireCross').on('click',function(){
+
+		if(confirm("Voulez-vous vraiment supprimer ce copropriétaire ?")){
+
+			idCoproprietaire = $(this).attr('value')
+			$.ajax({
+				url: 'model/gestionnaire/supprimerCoproprietaire.php',
+				type: 'POST',
+				data: 'idCoproprietaire='+ idCoproprietaire,
+				success : function(response){
+					console.log(response)
+					window.location.reload()
+				},
+				error : function(response){
+					console.log(response)
+				}
+			})
+		}
+	})
+
+	/* Click sur bouton modifier d'un copropriétaire*/
+	$('.modifierCoproprietaireLine').on('click',function(){
+		idCoproprietaire = $(this).attr('value')
+		window.location = "index.php?act=modifierCoproprietaireForm&id="+idCoproprietaire;
+	})
+	
+
+	/* Click sur bouton modifier d'un bien*/
+	$('.modifierBienLine').on('click',function(){
+		idBien = $(this).attr('value')
+		window.location = "index.php?act=modifierBienForm&id="+idBien;
+	})
+
+
+	/* Click sur bouton supprimer d'un copropriétaire*/
+	$('.supprimerBienCross').on('click',function(){
+
+		if(confirm("Voulez-vous vraiment supprimer ce copropriétaire ?")){
+
+			idBien = $(this).attr('value')
+			console.log(idBien)
+			$.ajax({
+				url: 'model/gestionnaire/supprimerBien.php',
+				type: 'POST',
+				data: 'idBien='+ idBien,
+				success : function(response){
+					console.log(response)
+					window.location.reload()
+				},
+				error : function(response){
+					console.log(response)
+				}
+			})
+		}
+	})
+
 });
