00a2fa79e4f422755c407410c7df1b350b729432
BeJulien <47186089+BeJulien@users.noreply.github.com>
2021-01-23 22:04:04 +0100

    adminScript.js (ajax ...)

--- js/adminScript.js
+++ js/adminScript.js
@@ -1,2 +1,55 @@
 $( document ).ready(function() {
+
+    let $deleteCroixGestionnaire = $('.deleteCroixGestionnaire') // Tableau de tous les boutons de suppression (gestionnaires)
+    let $modifEngrenageGestionnaire = $('.modifEngrenageGestionnaire') // Tableau de tous les boutons de modification (gestionnaires)
+    
+    let $deleteCroixCopropriete = $('.deleteCroixCopropriete') // Tableau de tous les boutons de suppression (copropriétés)
+    let $modifEngrenageCopropriete = $('.modifEngrenageCopropriete') // Tableau de tous les boutons de suppression (copropriétés)
+
+    $deleteCroixGestionnaire.each((index, element) => {
+        $(element).on('click', () => {
+            let idUtilisateur = $(element).attr('id')
+
+            $.ajax({
+                url: 'model/admin/supprGestionnaire.php',
+                type: 'POST',
+                data: 'suppr=' + idUtilisateur,
+                success: function() {
+                    location.reload()
+                }
+            })
+        })
+    })
+
+    $modifEngrenageGestionnaire.each((index, element) => {
+        $(element).on('click', () => {
+            let idUtilisateur = $(element).attr('id')
+
+            window.location = 'index.php?act=modifierGestionnaireForm&modif=' + idUtilisateur
+        })
+    })
+
+    $deleteCroixCopropriete.each((index, element) => {
+        $(element).on('click', () => {
+            let idCopropriete = $(element).attr('id')
+
+            $.ajax({
+                url: 'model/admin/supprCopropriete.php',
+                type: 'POST',
+                data: 'suppr=' + idCopropriete,
+                success: function() {
+                    location.reload()
+                }
+            })
+        })
+    })
+
+    $modifEngrenageCopropriete.each((index, element) => {
+        $(element).on('click', () => {
+            let idCopropriete = $(element).attr('id')
+
+            window.location = 'index.php?act=modifierCoproprieteForm&modif=' + idCopropriete
+        })
+    })
+
 });
