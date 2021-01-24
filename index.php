ca9993985236b5212ca2ee55599a998c99bfbe8a
johan31220 <58666354+johan31220@users.noreply.github.com>
2021-01-24 01:08:25 +0100

    Js balise

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ include('view/components/footer.html');
 
 
 <script type="text/javascript" src="js/jquery-3.5.1.min.js"></script>
+<script type="text/javascript" src="js/gestionnaireScript.js"></script>
 <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
