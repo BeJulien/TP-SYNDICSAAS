0243c76b8ea1e41f6bdc26b40b0119bb8bf2dcfe
BeJulien <47186089+BeJulien@users.noreply.github.com>
2021-01-24 09:30:51 +0100

    vues copropriétaire à finir

--- view/coproprietaireView/headerCoproprietaire.php
+++ view/coproprietaireView/headerCoproprietaire.php
@@ -3,10 +3,10 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
               <li class="nav-item active">
-                <div><a class="nav-link" href="#"><button class="btn btn-success btnHome"><i class="fas fa-home"></i> Accueil</button></a></div>
+                <div><a class="nav-link" href="index.php?act=accueil"><button class="btn btn-success btnHome"><i class="fas fa-home"></i> Accueil</button></a></div>
               </li>
               <li class="nav-item">
-                <div><a class="nav-link" href="#"><button class="btn btnNav">Remonter une information</button></a></div>
+                <div><a class="nav-link" href="index.php?act=remonteeInformation"><button class="btn btnNav">Remonter une information</button></a></div>
               </li>
               <li class="nav-item">
                 <div><a class="nav-link" href="index.php?act=deconnexion"><button class="btn btn-danger btnExit"><i class="fas fa-sign-out-alt"></i> Déconnexion</button></a></div>
---
+++ view/coproprietaireView/remonteeInformation.php
@@ -0,0 +1,1 @@
+<?php echo("remontée") ?>
