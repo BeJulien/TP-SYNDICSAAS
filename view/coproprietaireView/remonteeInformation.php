9332837328b606aa53a9629ce89bb8cb5fd58537
BeJulien <47186089+BeJulien@users.noreply.github.com>
2021-01-24 10:29:13 +0100

    modification vues copropriétaire

--- view/coproprietaireView/remonteeInformation.php
+++ view/coproprietaireView/remonteeInformation.php
@@ -1,1 +1,16 @@
-<?php echo("remontée") ?>
+<h1 class="titrePage">Remonter une information</h1>
+<form method="POST" action="model/coproprietaire/ajoutRemonteeInformations.php">
+	<!-- Formulaire inputs -->
+<div class="row mx-auto">
+	<div class="col-md-6 col-sm-4 mx-auto formulaireGestionInput">
+			<div class="form-group">
+				<textarea class="form-control" name="description" placeholder="Description" rows="4" required></textarea>
+			</div>
+	</div>
+</div>
+
+<div class="btForm"><button type="submit" class="btn btn-warning">Remonter l'information</button></div>
+</form>
+<div>
+<a class="nav-link" href="index.php?act=accueil"><button type="submit" class="btn btn-danger d-block mx-auto">Annuler</button></a>
+</div>
